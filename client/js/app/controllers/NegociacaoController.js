e71577776eb643860b7ae665aa1c9dbe0aceed4b
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-03-25 22:30:24 -0300

    Created ListaNegociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,11 +6,14 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
-        const negociacao = new Negociacao (DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
+        const negociacao = this._criaNegociacao();
+        console.log(negociacao);
+        this._listaNegociacoes.adiciona(negociacao);
         this.limpaCampos();
     }
 
@@ -19,5 +22,9 @@ class NegociacaoController{
         this._inputQuantidade.value = '1';
         this._inputValor.value = '0.0'
         this._inputData.focus();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao (DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
     }
 }
